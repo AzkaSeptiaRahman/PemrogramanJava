ecd65d91acb8c6c92d107edd39116be6f22220d3
Azka Septia Rahman <74814282+AzkaSeptiaRahman@users.noreply.github.com>
2021-03-29 07:51:34 +0700

    Delete InheritanceMatematika directory

--- InheritanceMatematika/MatematikaCanggihBanget.java
+++
@@ -1,8 +0,0 @@
-package InheritanceMatematika;
-
-public class MatematikaCanggihBanget extends MatematikaCanggih{
-    void PertambahanTiga(int a, int b, int c){
-        operasi = a + b + c ;
-        System.out.println("Hasil Pertambahan Tiga adalah = "+getOperasi());
-    }
-}
